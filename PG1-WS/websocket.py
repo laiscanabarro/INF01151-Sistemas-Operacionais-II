9cd54ad7ab96e3010861f27f7c126b8ec79e8d08
laiscanabarro <laisaraujo1312@gmail.com>
2025-05-15 18:58:35 -0300

    Add chat method

--- PG1-WS/websocket.py
+++ PG1-WS/websocket.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 import websockets
 
 from asyncio import run
@@ -12,10 +13,13 @@ async def http_handler(path, headers):
     from http import HTTPStatus
     from websockets.http import Headers
 
+    #base_dir = os.path.dirname(os.path.abspath(__file__))
+
     if path == '/ui-chat':
         # Entregar para o navegador o conteúdo do arquivo chat.html,
         # que corresponde ao cliente chat implementado pelo webservice
         with open("chat.html") as f:
+        #with open(os.path.join(base_dir, "chat.html")) as f:
             headers = Headers(**{'Content-Type': 'text/html'})
             body = bytes(f.read(), 'utf-8')
 
@@ -25,6 +29,7 @@ async def http_handler(path, headers):
         # Entregar para o navegador o conteúdo do arquivo echo.html,
         # que corresponde ao cliente echo implementado pelo webservice
         with open("echo.html") as f:
+        #with open(os.path.join(base_dir, "echo.html")) as f:
             headers = Headers(**{'Content-Type': 'text/html'})
             body = bytes(f.read(), 'utf-8')
 
@@ -39,6 +44,29 @@ async def echo(websocket):
     async for message in websocket:
         await websocket.send(message)
 
+async def chat(websocket, sessions={}):
+    """Chat WebSocket handler"""
+    remote = websocket.remote_address
+    sessions[remote] = websocket
+
+    logs_message = f"Cliente {remote} se conectou"
+    for socket in sessions.values():
+        await socket.send(logs_message)
+
+    try:
+        async for message in websocket:
+            await websocket.send(message)
+            logs_message = f"Cliente {remote} enviou: {message}"
+            for socket in sessions.values():
+                if socket != websocket:
+                    await socket.send(logs_message)
+
+    finally:
+        if remote in sessions:
+            del sessions[remote]
+        logs_message = f"Cliente {remote} se desconectou"
+        for socket in sessions.values():
+            await socket.send(logs_message)
 
 async def web_socket_router(websocket, path):
     """Route WebSocket requests to their handlers"""
@@ -46,6 +74,8 @@ async def web_socket_router(websocket, path):
         await websocket.close(reason=f'needs a path')
     elif path == '/echo':
         await echo(websocket)
+    elif path == '/chat':
+        await chat(websocket)
     else:
         await websocket.close(reason=f'path not found: {path}')
 
